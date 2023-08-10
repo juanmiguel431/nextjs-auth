2646cd4a8ef6d95cb04a487b560d58e3da06d613
juanmiguel431 <juanmiguel431@gmail.com>
2023-08-09 23:35:08 -0400

    Old password first than new password.

--- src/components/profile/profile-form.tsx
+++ src/components/profile/profile-form.tsx
@@ -27,12 +27,12 @@ const ProfileForm: React.FC<ProfileFormProps> = (props) => {
   return (
     <form className={classes.form} onSubmit={e => submitHandler}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' ref={newPasswordRef} />
-      </div>
-      <div className={classes.control}>
         <label htmlFor='old-password'>Old Password</label>
         <input type='password' id='old-password' ref={oldPasswordRef} />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor='new-password'>New Password</label>
+        <input type='password' id='new-password' ref={newPasswordRef} />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
